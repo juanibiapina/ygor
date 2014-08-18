2a9cc57b40fd5d0350884a2f70444494f8476cee
Juan Ibiapina <juanibiapina@gmail.com>
2014-08-17 22:52:46 -0300

    Add the REPL.

--- main.rkt
+++ main.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(provide (rename-out [ygor-module-begin #%module-begin]))
+(provide #%top-interaction
+         (rename-out [ygor-module-begin #%module-begin]))
 
 (define-syntax-rule (ygor-module-begin body ...)
   (#%plain-module-begin body ...))
