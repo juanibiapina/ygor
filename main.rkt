c47ca0428c7a3b3d8c022fe08b69dfbd2a44ee57
Juan Ibiapina <juanibiapina@gmail.com>
2014-08-17 22:12:21 -0300

    Separate provides to match blog post.

--- main.rkt
+++ main.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
-(provide hello?
-         (rename-out [ygor-module-begin #%module-begin]))
+(provide (rename-out [ygor-module-begin #%module-begin]))
 
 (define-syntax-rule (ygor-module-begin body ...)
   (#%plain-module-begin body ...))
+
+(provide hello?)
 
 (define-syntax-rule (hello?)
   (print "hello to you too!"))
