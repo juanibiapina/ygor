9ea47b8fec6d5ce8a389154365e50e7e69bb44de
Juan Ibiapina <juanibiapina@gmail.com>
2014-10-03 15:56:58 -0300

    First working version for blog post.

--- main.rkt
+++ main.rkt
@@ -1,12 +1,33 @@
 #lang racket
 
 (provide #%top-interaction
+         #%app
+         #%datum
          (rename-out [ygor-module-begin #%module-begin]))
 
-(define-syntax-rule (ygor-module-begin body ...)
-  (#%plain-module-begin body ...))
+(define-syntax (ygor-module-begin stx)
+  (datum->syntax
+     stx
+     (cons (quote-syntax #%module-begin)
+           (map (lambda (e)
+                  (list (quote-syntax ygor-eval)
+                        e))
+                (cdr (syntax-e stx))))
+     stx
+     stx))
 
 (provide hello?)
 
 (define-syntax-rule (hello?)
   (print "hello to you too!"))
+
+(provide const)
+(struct const (v) #:transparent)
+
+(provide sum)
+(struct sum (e1 e2) #:transparent)
+
+(define (ygor-eval e)
+  (match e
+    [(const x) (const x)]
+    [(sum e1 e2) (const (+ (const-v (ygor-eval e1)) (const-v (ygor-eval e2))))]))
